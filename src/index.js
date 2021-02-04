04255830d3a07110f98cc48dcac83927df0c62ea
Andres <andreslh90@gmail.com>
2021-02-04 16:38:28 +0100

    feat: cors added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/api', routes);
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
 module.exports = app;
